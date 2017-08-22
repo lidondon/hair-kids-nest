a238cdb090beec8fe3219239df14a91eeb54f1ac
李建霖 <lidondon@lijianlinde-MacBook-Pro.local>
2017-08-22 10:19:32 +0800

    reset type_id to int for heroku

--- db/migrate/20170821160026_reset_type_in_pet.rb
+++ db/migrate/20170821160026_reset_type_in_pet.rb
@@ -1,6 +1,6 @@
 class ResetTypeInPet < ActiveRecord::Migration[5.1]
   def change
   		rename_column :pets, :type, :type_id
-  		change_column :pets, :type_id, :integer
+  		change_column :pets, :type_id, 'integer USING CAST("type_id" AS integer)'
   end
 end
